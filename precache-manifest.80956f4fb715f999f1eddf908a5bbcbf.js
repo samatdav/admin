6708ee07b5b102c43bfd89abed66b0c2b9c470ab
samatdav <samatdav@gmail.com>
2019-02-21 18:46:42 -0600

    Updates

---
+++ precache-manifest.80956f4fb715f999f1eddf908a5bbcbf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "22cf4ea0439f8c5456ba",
+    "url": "/admin/static/css/main.afe89654.chunk.css"
+  },
+  {
+    "revision": "22cf4ea0439f8c5456ba",
+    "url": "/admin/static/js/main.22cf4ea0.chunk.js"
+  },
+  {
+    "revision": "62c0476b864e65bbf9f1",
+    "url": "/admin/static/js/runtime~main.62c0476b.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/admin/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/admin/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c3d2156d7e283b611c60648c07a3fd19",
+    "url": "/admin/index.html"
+  }
+];
